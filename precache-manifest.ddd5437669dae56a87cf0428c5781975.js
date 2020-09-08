b1aa05c74e5fecf13557deddd34880b326a569a3
Euna789 <55429220+Euna789@users.noreply.github.com>
2020-09-08 18:10:16 +0900

    Updates

---
+++ precache-manifest.ddd5437669dae56a87cf0428c5781975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d55f097cd71d01df0f2ce9ef50e4e2",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "1e61b00e1f27f90163cc",
+    "url": "/movie_app/static/css/main.459df648.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e61b00e1f27f90163cc",
+    "url": "/movie_app/static/js/main.7c8cfd33.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
